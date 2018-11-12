e2aadb84402909a156dd546788baa83fdc2b373e
simon <simon.kruecken@stud.hn.de>
2018-11-12 13:37:49 +0100

    rename

---
+++ sdl_game/cmake-build-debug/CMakeFiles/sdl_game.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sdl_game.dir/src/main.cpp.o"
+  "sdl_game.pdb"
+  "sdl_game"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sdl_game.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
