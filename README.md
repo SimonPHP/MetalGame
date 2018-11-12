531cadcc7c433404e160a48a1fdf0623847b3bcf
simon <simon.kruecken@stud.hn.de>
2018-11-12 12:32:48 +0100

    init

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # MetalGame
 2D C++ Spieleentwicklungs Projekt
+
+## Spiel Idee
+
+### Intro
+Man sitzt im Bus und hört Musik.
+Nun kann man ein lied auswählen.
+Zu diesem Lied träumt der Charakter dann entsprechends.
+
+Nun wird ein Level geladen was der Musik entspricht.
+
+### Spiel
+Man Spielt einen Charakter aus der entsprechenden Metal Band und muss ein entsprechendes gegen genre bekämpfen z.B. Disco Musik.
+Der endboss ist dann immer ein Charakter aus einer Band des Genres.
+
+### Charakter
+Jeder Charakter hat 3 Moves, Meele, Range, Ulti.
+Die Waffe eines Charakters ist sein Instrument.
+
+## Umsetzung
+Es wird ein 2D Sidescroller und Platformer.
+
+Ich denke mal wir verwenden am besten den Wrapper (suck it Jan :P).
---
+++ sdl_game-master/src/main.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <stdio.h>
+
+#include "global.h"
+
+#include "gamebase.h"
+#include "intro.h"
+#include "editor.h"
+
+class MyGame : public Game
+{
+public:
+	MyGame()
+		: Game( Rect( 1024, 768 ), false )
+	{
+		allStates[0] = new IntroState( *this, renderer );
+		allStates[1] = new EditorState( *this, renderer );
+		SetNextState( 0 );
+	}
+
+	virtual ~MyGame()
+	{
+		for( auto state : allStates )
+			if( state != nullptr )
+				delete state;
+	}
+};
+
+extern "C"
+int main( int argc, char* argv[] )
+try
+{
+	MyGame game;
+	return game.Run();
+}
+catch( SDL::Exception& e )
+{
+	std::cout
+		<< "Aborting program: " << e.Text()
+		<< " error code: " << e.Code() << std::endl;
+
+	// put a breakpoint in this line, to prevent the shell from closing
+	return e.Code();
+}
+catch( ... )
+{
+	std::cout << "Non-SDL Exception has occured!" << std::endl;
+	// 
+	// put a breakpoint in this line, to prevent the shell from closing
+	return -999;
+}
